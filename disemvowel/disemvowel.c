35e3990442f346365a7e5589f1c0139795ad1af4
Ben <burge337@morris.umn.edu>
2020-09-17 13:35:15 -0500

    Providing comments for the disemvowel and main.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -9,17 +9,25 @@ char *disemvowel(char *str) {
 
 	vowels = 0;
 	len = strlen(str);
-
+        // for loop to count the number of vowels the input has
 	for (i=0; i<len; ++i) {
 	if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E' || str[i] == 'i' ||
 			str[i] == 'I' || str[i] == 'o' || str[i] == 'O' || str[i] == 'u' || str[i] == 'U'){
 	vowels = vowels + 1;
 	}}
 	// the for loop above this should probably be a helper function
-
+        
+	// number of consonants/other characters = length - # of vowels
+	// important to make sure we allocate only the memory we need
 	nlength = len - vowels;
+
+	// counter variable
 	r = 0;
+        
+        // memory allocation for our disemvoweled input
 	newword = (char*) calloc(nlength+1, sizeof(char));
+
+	// whenever we encounter non-vowels, throw them into our array of newly allocated memory
 	for(i=0; i<len; ++i){
 	if (str[i] != 'a' && str[i] != 'A' && str[i] != 'e' && str[i] != 'E' && str[i] != 'i' &&
 		str[i] != 'I' && str[i] != 'o' && str[i] != 'O' && str[i] != 'u' && str[i] != 'U'){
@@ -27,7 +35,10 @@ char *disemvowel(char *str) {
 	++r;
 	}
 	}
+	// slap on the null terminator that we need at the end
 	newword[nlength]= '\0';
 	
+  // send that pointer right on back to whoever called this.
+  // They will be responsible for freeing this memory
   return newword;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -12,11 +12,12 @@ int main(int argc, char *argv[]) {
   line = (char*) malloc (size + 1);
 
   while (getline(&line, &size, stdin) > 0) {
-    //memory needs to be freed in disemvowel, but we can't do so until the end of our while loop.
-    //problem with that is disemvowel doesn't know when it won't be called anymore.
+    // capture the pointer to our memory that disemvowel returns
     disemvoweled = disemvowel(line);
     printf("%s\n", disemvoweled);
+    // free the memory that disemvowel returns
     free(disemvoweled);
   }
+  // free the memory that was passed to disemvowel
   free(line);
 }
