c4b0c70f2074776252f0c4ebcdbfe699ee881c5a
halleypaulson <newprofessionalism@gmail.com>
2020-09-17 09:46:29 -0500

    Started disemvowel, not working

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,5 +3,24 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+	int len, i, count, r;
+	char *newword;
+
+	count = 0;
+	len = strlen(str);
+
+	for (i=0; i<len; ++i) {
+	if (str[i] == ("a" | "A" | "e" | "E" | "i" | "I" | "o" | "O" | "u" | "U")){
+	count = count + 1;
+	}}
+
+	r = 0;
+	newword = (char*) calloc(count+1, sizeof(char));
+	for(i=0; i<count+1; ++i){
+	if (str[i] != ("a" | "A" | "e" | "E" | "i" | "I" | "o" | "O" | "u" | "U")){
+	newword[r] = str[i];
+	++r;
+	}}
+	
+  return newword";
 }
